110019d200ee0e5e76b4abb24963e279a0476953
Aleksandr Semenov <semenlinux@gmail.com>
2021-03-14 22:11:39 +0300

    Make the code more reliable
    - Add Last Page number definition.
    - Do not interrupt the download even if one book is not available.
    - Process only if there is a link to download txt.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -15,13 +15,12 @@ def create_input_parser():
     parser.add_argument(
         "--start_page",
         type=int,
-        required=True,
+        default=1,
         help="Первая страница",
     )
     parser.add_argument(
-        "--end_page",
+        "--last_page",
         type=int,
-        default=702,
         help="Последняя страница",
     )
     parser.add_argument(
@@ -57,13 +56,14 @@ def fetch_response(link: str) -> requests.models.Response:
     return link_response
 
 
-def parse_category_page(html: str) -> list:
+def parse_category_page(html: str, website_link: str) -> list:
     html_soup = BeautifulSoup(html, "lxml")
     book_link_selector = "body div#content .d_book tr:nth-of-type(2) a"
-    book_short_links = [
-        book_link["href"] for book_link in html_soup.select(book_link_selector)
+    book_links = [
+        get_full_link(website_link, book_link["href"])
+        for book_link in html_soup.select(book_link_selector)
     ]
-    return book_short_links
+    return book_links
 
 
 def get_full_link(website_link: str, short_link: str) -> str:
@@ -84,51 +84,52 @@ def extract_file_extension(link: str) -> str:
     return file_extension
 
 
-def parse_book_page(website_link: str, book_short_link: str) -> dict:
-    book_description = {}
-    book_link = get_full_link(website_link, book_short_link)
-    book_link_response = fetch_response(book_link)
-    html_soup = BeautifulSoup(book_link_response.content, "lxml")
+def fetch_book_text_link(html: str) -> str:
+    html_soup = BeautifulSoup(html, "lxml")
     book_text_link_selector = "body div#content .d_book a[title$='скачать книгу txt']"
     book_text_link = html_soup.select_one(book_text_link_selector)
-    if book_text_link:
-        book_title_and_author_selector = "body div#content h1"
-        book_title_and_author = html_soup.select_one(book_title_and_author_selector)
-        book_title, book_author = book_title_and_author.text.split("::")
+    return book_text_link
 
-        book_cover_link_selector = "body .bookimage img"
-        book_cover_link = html_soup.select_one(book_cover_link_selector)
 
-        book_comment_selector = "body div#content .texts .black"
-        book_comments = [
-            book_comment.text
-            for book_comment in html_soup.select(book_comment_selector)
-        ]
+def parse_book_page(html: str) -> dict:
+    book_description = {}
+    html_soup = BeautifulSoup(html, "lxml")
+    book_title_and_author_selector = "body div#content h1"
+    book_title_and_author = html_soup.select_one(book_title_and_author_selector)
+    book_title, book_author = book_title_and_author.text.split("::")
 
-        book_genre_selector = "body div#content span.d_book a"
-        book_genres = [
-            book_genre.text for book_genre in html_soup.select(book_genre_selector)
-        ]
+    book_cover_link_selector = "body .bookimage img"
+    book_cover_link = html_soup.select_one(book_cover_link_selector)
 
-        book_description = {
-            "title": book_title.strip(),
-            "author": book_author.strip(),
-            "img_src_link": book_cover_link["src"],
-            "book_path_link": book_text_link["href"],
-            "genres": book_genres,
-            "comments": book_comments,
-        }
+    book_comment_selector = "body div#content .texts .black"
+    book_comments = [
+        book_comment.text for book_comment in html_soup.select(book_comment_selector)
+    ]
+
+    book_genre_selector = "body div#content span.d_book a"
+    book_genres = [
+        book_genre.text for book_genre in html_soup.select(book_genre_selector)
+    ]
+
+    book_description = {
+        "title": book_title.strip(),
+        "author": book_author.strip(),
+        "img_src_link": book_cover_link["src"],
+        "genres": book_genres,
+        "comments": book_comments,
+    }
     return book_description
 
 
-def download_book_text(category_link: str, book_description: dict, folder: str) -> str:
-    book_text_link = get_full_link(category_link, book_description["book_path_link"])
-    book_text_filename = book_description["title"]
-    sanitized_filename = sanitize_filename(book_text_filename)
-    page_response = fetch_response(book_text_link)
+def download_book_text(
+    category_link: str, text_link: dict, folder: str, filename: str
+) -> str:
+    book_text_link = get_full_link(category_link, text_link)
+    sanitized_filename = sanitize_filename(filename)
+    response = fetch_response(book_text_link)
     filepath = f"{ os.path.join(folder, sanitized_filename) }.txt"
     with open(filepath, "w") as file:
-        file.write(page_response.text)
+        file.write(response.text)
     return filepath
 
 
@@ -136,25 +137,33 @@ def download_cover(category_link: str, book_description: dict, folder: str) -> s
     book_cover_link = get_full_link(category_link, book_description["img_src_link"])
     book_cover_extension = extract_file_extension(book_cover_link)
     book_cover_filename = f"{ book_description['title'] }{ book_cover_extension }"
-    page_response = fetch_response(book_cover_link)
+    response = fetch_response(book_cover_link)
     sanitized_filename = sanitize_filename(book_cover_filename)
     filepath = os.path.join(folder, sanitized_filename)
     with open(filepath, "wb") as file:
-        file.write(page_response.content)
+        file.write(response.content)
     return filepath
 
 
-def get_book_short_links(
+def get_book_links(
     category_link: str, start_category_page: int, end_category_page: int
 ) -> list:
-    book_short_links = []
+    book_links = []
     for page in range(start_category_page, end_category_page):
         category_page_link = urljoin(category_link, str(page))
         category_page_link_response = fetch_response(category_page_link)
-        book_short_links = book_short_links + parse_category_page(
-            category_page_link_response.content
+        book_links = book_links + parse_category_page(
+            category_page_link_response.content, category_link
         )
-    return book_short_links
+    return book_links
+
+
+def fetch_category_last_page(category_link: str) -> int:
+    response = fetch_response(category_link)
+    html_soup = BeautifulSoup(response.content, "lxml")
+    category_last_page_selector = "body div#content p.center a:last-child"
+    category_last_page = html_soup.select_one(category_last_page_selector)
+    return int(category_last_page.text)
 
 
 def main():
@@ -172,28 +181,42 @@ def main():
     Path(json_folder).mkdir(parents=True, exist_ok=True)
     book_description_filepath = os.path.join(json_folder, json_filename)
     try:
-        book_short_links = get_book_short_links(
-            tululu_category_link, args.start_page, args.end_page
+        if not args.last_page:
+            args.last_page = fetch_category_last_page(tululu_category_link)
+        book_links = get_book_links(
+            tululu_category_link, args.start_page, args.last_page
         )
-        for book_short_link in book_short_links:
-            book_description = parse_book_page(tululu_category_link, book_short_link)
-            if "book_path_link" in book_description:
-                if not args.skip_imgs:
-                    book_description["img_src"] = download_cover(
-                        tululu_category_link, book_description, cover_folder
-                    )
-                if not args.skip_txt:
-                    book_description["book_path"] = download_book_text(
-                        tululu_category_link, book_description, book_folder
-                    )
-                book_description.pop("img_src_link", None)
-                book_description.pop("book_path_link", None)
-                book_description_json.append(book_description)
-        write_file_json(book_description_json, book_description_filepath)
-    except requests.exceptions.ConnectionError:
-        print("Что-то пошло не так:( Проверьте подключение к интернету!")
-    except requests.exceptions.HTTPError:
-        print("Проблемы с доступом к сайту tululu.org")
+    except (
+        requests.exceptions.ConnectionError,
+        requests.exceptions.HTTPError,
+    ) as error:
+        print(f"Произошла ошибка: { error }")
+    for book_link in book_links:
+        try:
+            response = fetch_response(book_link)
+            text_link = fetch_book_text_link(response.content)
+            if not text_link:
+                continue
+            book_description = parse_book_page(response.content)
+            if not args.skip_imgs:
+                book_description["img_src"] = download_cover(
+                    tululu_category_link, book_description, cover_folder
+                )
+            if not args.skip_txt:
+                book_description["book_path"] = download_book_text(
+                    tululu_category_link,
+                    text_link["href"],
+                    book_folder,
+                    book_description["title"],
+                )
+            book_description.pop("img_src_link", None)
+            book_description_json.append(book_description)
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.HTTPError,
+        ) as error:
+            print(f"Произошла ошибка: { error }")
+    write_file_json(book_description_json, book_description_filepath)
 
 
 if __name__ == "__main__":
